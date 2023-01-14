304cc2074819b68f32ce03efb24fed7e7c74f31b
Varun Dhawan <varundhawan441@gmail.com>
2023-01-14 14:35:37 +0530

    Add validation in Create Category page

--- Pages/Categories/Create.cshtml.cs
+++ Pages/Categories/Create.cshtml.cs
@@ -6,15 +6,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FoodieWeb.Pages.Categories
 {
-//	[BindProperties]
-	public class CreateModel : PageModel
+    //	[BindProperties]
+    public class CreateModel : PageModel
     {
-        
+
         public Category Category { get; set; }
         private readonly ApplicationDbContext dbContext;
         public CreateModel(ApplicationDbContext context)
         {
-            dbContext= context;
+            dbContext = context;
         }
         public void OnGet()
         {
@@ -22,9 +22,17 @@ namespace FoodieWeb.Pages.Categories
 
         public async Task<IActionResult> OnPost(Category category)
         {
-            await dbContext.Categories.AddAsync(category);
-            await dbContext.SaveChangesAsync();
-            return RedirectToPage("Index");
+            if(category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(string.Empty, "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                await dbContext.Categories.AddAsync(category);
+                await dbContext.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
         }
     }
 }
